77840d185dfc3d4eab223e42c3f71587e0703b4f
Yoann Trellu <yoann.trellu@yahoo.com>
2021-03-06 13:24:43 +0000

    corrections and added introspection link

--- named/control.sh
+++ named/control.sh
@@ -10,10 +10,10 @@ STORED_IP=`echo $DNS_DATA | cut -d ',' -f 1 | tr -d '"' | cut -d ":" -f 2`
 
 echo "`date '+%Y-%m-%d %H:%M:%S'` - Current External IP is $CURRENT_IP, GoDaddy DNS IP is $STORED_IP"
 
-if [ "$STORED_IP" != "$CURRENT_IP" -a "$myip" != "" ]; then
+if [ "$STORED_IP" != "$CURRENT_IP" ]; then
 	  echo "IP has changed!! Updating on GoDaddy"
     curl -s -X PUT "https://api.godaddy.com/v1/domains/${DOMAIN}/records/A/${HOST_NAME}" \
-         -H "Authorization: sso-key ${API_KEY}" \
+         -H "Authorization: sso-key ${GO_DADDY_API_KEY}" \
          -H "Content-Type: application/json" \
          -d "[{\"data\": \"${CURRENT_IP}\"}]"
 	  logger -p $LOG_DEST "Changed IP on ${hostname}.${DOMAIN} from ${STORED_IP} to ${CURRENT_IP}"
